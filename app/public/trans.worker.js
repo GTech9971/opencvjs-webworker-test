966e81f06abd2eaee621f83c0beddf61a5912bd1
DJ <georgetech1984@gmail.com>
2023-02-16 22:41:32 +0900

    webworkerを通して、射影変換をopencvjsで行うことに成功

--- app/public/trans.worker.js
+++ app/public/trans.worker.js
@@ -1,4 +1,5 @@
 function waitForOpencv(callbackFn, waitTimeMs = 30000, stepTimeMs = 100) {
+    // eslint-disable-next-line no-undef
     if (cv.Mat) callbackFn(true)
 
     let timeSpentMs = 0
@@ -13,59 +14,80 @@ function waitForOpencv(callbackFn, waitTimeMs = 30000, stepTimeMs = 100) {
     }, stepTimeMs)
 }
 
-
 self.onmessage = (event) => {
-    self.importScripts('./opencv.js');
-    waitForOpencv(success => {
-        //self.postMessage(success);
-        const img = cv.matFromImageData(event.data);
-        const dst = new cv.Mat();
+    const { type, data } = event.data;
+    switch (type) {
+        case 'init': {
+            self.importScripts('./opencv.js');
+            waitForOpencv(success => {
+                self.postMessage({ type: type, data: `init :${success}` });
+            });
+            break;
+        } case 'putImg': {
+            const img = cv.matFromImageData(data);
+            const dst = new cv.Mat();
 
-        cv.cvtColor(img, dst, cv.COLOR_BGR2RGB, 4)
-        console.log(dst.channel);
+            cv.cvtColor(img, dst, cv.COLOR_BGR2RGB, 4)
 
-        const imgData = new ImageData(
-            new Uint8ClampedArray(dst.data),
-            dst.cols,
-            dst.rows
-        );
-        img.delete();
-        dst.delete();
-        self.postMessage(imgData);
-    });
-    console.log(event);
+            const imgData = mat2ImgData(dst);
+            img.delete();
+            dst.delete();
+            self.postMessage({ type: type, data: imgData });
+            break;
+        }
+        case 'trans': {
+            const src = cv.matFromImageData(data);
+            const fourCorners = [{ x: 249, y: 113 }, { x: 822, y: 177 }, { x: 798, y: 514 }, { x: 222, y: 550 }];
+            const dst = transform(src, fourCorners);
+
+            const imgData = mat2ImgData(dst);
+            src.delete();
+            dst.delete();
+
+            self.postMessage({ type: type, data: imgData });
+            break;
+        } default: {
+            break;
+        }
+    }
 }
 
+const mat2ImgData = (src) => {
+    return new ImageData(
+        new Uint8ClampedArray(src.data),
+        src.cols,
+        src.rows
+    );
+}
 
+const transform = (src, fourCorners) => {
+    const ratio = 1.2;
+    const p0 = fourCorners[0]
+    const p1 = fourCorners[1];
+    const p2 = fourCorners[2];
+    const p3 = fourCorners[3];
 
-// const transform = (src: Mat, fourCorner: Point[]): Mat => {
-//     const ratio: number = 1.2;
-//     const p0: Point = fourCorner[0];
-//     const p1: Point = fourCorner[1];
-//     const p2: Point = fourCorner[2];
-//     const p3: Point = fourCorner[3];
+    let width = Math.sqrt(Math.pow(p1.x - p0.x, 2) + Math.pow(p1.y - p0.y, 2));
+    width = Math.floor(width * ratio);
 
-//     let width: number = Math.sqrt(Math.pow(p1.x - p0.x, 2) + Math.pow(p1.y - p0.y, 2));
-//     width = Math.floor(width * ratio);
+    let height = Math.sqrt(Math.pow(p2.x - p0.x, 2) + Math.pow(p2.y - p0.y, 2));
+    height = Math.floor(height * ratio);
 
-//     let height: number = Math.sqrt(Math.pow(p2.x - p0.x, 2) + Math.pow(p2.y - p0.y, 2));
-//     height = Math.floor(height * ratio);
+    // (data32F[0], data32F[1]) is the top left
+    // (data32F[2], data32F[3]) is the top right
+    // (data32F[4], data32F[5]) is the bottom left
+    // (data32F[6], data32F[7]) is the bottom right
+    const srcQuad = cv.matFromArray(4, 1, cv.CV_32FC2, [p0.x, p0.y, p1.x, p1.y, p3.x, p3.y, p2.x, p2.y]);
+    const dstQuad = cv.matFromArray(4, 1, cv.CV_32FC2, [0, 0, width, 0, 0, height, width, height]);
 
-//     // (data32F[0], data32F[1]) is the top left
-//     // (data32F[2], data32F[3]) is the top right
-//     // (data32F[4], data32F[5]) is the bottom left
-//     // (data32F[6], data32F[7]) is the bottom right
-//     const srcQuad: Mat = cv.matFromArray(4, 1, cv.CV_32FC2, [p0.x, p0.y, p1.x, p1.y, p3.x, p3.y, p2.x, p2.y]);
-//     const dstQuad: Mat = cv.matFromArray(4, 1, cv.CV_32FC2, [0, 0, width, 0, 0, height, width, height]);
+    const wrapMat = cv.getPerspectiveTransform(srcQuad, dstQuad);
 
-//     const wrapMat: Mat = cv.getPerspectiveTransform(srcQuad, dstQuad);
+    const dst = new cv.Mat();
+    cv.warpPerspective(src, dst, wrapMat, new cv.Size(width, height), cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
 
-//     const dst: Mat = new cv.Mat();
-//     cv.warpPerspective(src, dst, wrapMat, new cv.Size(width, height), cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+    srcQuad.delete();
+    dstQuad.delete();
+    wrapMat.delete();
 
-//     srcQuad.delete();
-//     dstQuad.delete();
-//     wrapMat.delete();
-
-//     return dst;
-// }
+    return dst;
+}
--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,38 +1,71 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import { useRef, useState } from 'react';
 import './Home.css';
 
+export interface Message {
+  type: 'init' | 'putImg' | 'trans',
+  data: any,
+}
+
 const Home: React.FC = () => {
   const [worker, setWorker] = useState<Worker>(new Worker('./trans.worker.js'));
-  const [text, setText] = useState<string>('');
   const imgRef = useRef<HTMLImageElement>(null);
   const transImgRef = useRef<HTMLImageElement>(null);
 
+
+  worker.addEventListener('message', (event: any) => {
+    const { type, data } = event.data as Message;
+    switch (type) {
+      case 'init': {
+        console.log(data);
+        break;
+      }
+      case 'putImg':
+      case 'trans': {
+        if (!transImgRef.current) { return; }
+        const canvas: HTMLCanvasElement = document.createElement('canvas');
+        canvas.width = data.width
+        canvas.height = data.height
+        const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
+        ctx.putImageData(data, 0, 0);
+        transImgRef.current.src = canvas.toDataURL('image/png');
+        break;
+      }
+    }
+  });
+
+
   const onClickWebworkerBtn = () => {
-    worker.addEventListener('message', ({ data }) => {
-      console.log(data);
-      if (!transImgRef.current) { return; }
-      const canvas: HTMLCanvasElement = document.createElement('canvas');
-      canvas.width = data.width
-      canvas.height = data.height
-      const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
-      ctx.putImageData(data, 0, 0);
-      transImgRef.current.src = canvas.toDataURL('image/png');
-    });
     console.log('init worker');
+    const message: Message = { type: 'init', data: null };
+    worker.postMessage(message);
   }
 
   const onClickSendMessage = () => {
     if (!imgRef.current) { return; }
     const canvas: HTMLCanvasElement = document.createElement('canvas');
-    canvas.width = imgRef.current!.width
-    canvas.height = imgRef.current!.height
+    canvas.width = 1280
+    canvas.height = 720
     const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
     ctx.drawImage(imgRef.current, 0, 0);
 
     const data: ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    worker.postMessage(data);
+    const message: Message = { type: 'putImg', data: data };
+    worker.postMessage(message);
   }
+
+  const onClickTransform = () => {
+    if (!imgRef.current) { return; }
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
+    canvas.width = 1280
+    canvas.height = 720
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
+    ctx.drawImage(imgRef.current, 0, 0);
+
+    const data: ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const message: Message = { type: 'trans', data: data };
+    worker.postMessage(message);
+  };
 
   return (
     <IonPage>
@@ -44,12 +77,13 @@ const Home: React.FC = () => {
       <IonContent fullscreen>
         <IonButton onClick={onClickWebworkerBtn}>Start worker</IonButton>
         <IonItem>
-          <IonLabel>Input message</IonLabel>
-          <IonInput type='text'
-            value={text}
-            onIonInput={(e: any) => { setText(e.target.value) }}
-            placeholder='input send message' />
+          <IonLabel>RGB2BGR</IonLabel>
           <IonButton onClick={onClickSendMessage}>send message</IonButton>
+        </IonItem>
+
+        <IonItem>
+          <IonLabel>Image transform</IonLabel>
+          <IonButton onClick={onClickTransform}>Transform</IonButton>
         </IonItem>
 
         <IonGrid>
